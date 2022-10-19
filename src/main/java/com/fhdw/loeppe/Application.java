1cb824f7dc83cd7472ec4a5c980d2fcf059c3ede
heppitim <timheppi@hotmail.de>
2022-10-19 15:20:42 +0200

    Application

--- src/main/java/com/fhdw/loeppe/Application.java
+++ src/main/java/com/fhdw/loeppe/Application.java
@@ -1,10 +1,16 @@
 package com.fhdw.loeppe;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
+import com.vaadin.flow.theme.material.Material;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@Theme(variant = Lumo.LIGHT)
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
