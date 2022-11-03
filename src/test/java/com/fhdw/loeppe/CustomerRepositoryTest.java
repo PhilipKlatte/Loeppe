08a4a9bf01a273ae334636497436cd4c27a37d15
Philip <klattephilip@gmail.com>
2022-11-03 10:35:26 +0100

    saveCustomerSuccess() implementiert

--- src/test/java/com/fhdw/loeppe/CustomerRepositoryTest.java
+++ src/test/java/com/fhdw/loeppe/CustomerRepositoryTest.java
@@ -1,4 +1,31 @@
 package com.fhdw.loeppe;
 
+import com.fhdw.loeppe.entity.CustomerEntity;
+import com.fhdw.loeppe.repo.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
 public class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository repository;
+
+    @Test
+    public void saveCustomerSuccess(){
+        CustomerEntity entity = new CustomerEntity("John", "Doe", "Berlin");
+        repository.save(entity);
+
+        var result = repository.findById(1L);
+
+        assertTrue(result.isPresent());
+        assertEquals(result.get().getId(), 1L);
+        assertEquals(result.get().getFirstname(), "John");
+        assertEquals(result.get().getLastname(), "Doe");
+        assertEquals(result.get().getAddress(), "Berlin");
+    }
 }
