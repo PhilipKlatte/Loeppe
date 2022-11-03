303d459de2719b54186efe6c15392ea0e8e7d9b9
Philip <klattephilip@gmail.com>
2022-11-03 12:09:00 +0100

    @BeforeEach und @AfterEach implementiert; Kapselung noch nicht adäquat

--- src/test/java/com/fhdw/loeppe/repositories/ArticleRepositoryTest.java
+++ src/test/java/com/fhdw/loeppe/repositories/ArticleRepositoryTest.java
@@ -1,15 +1,14 @@
 package com.fhdw.loeppe.repositories;
 
 import com.fhdw.loeppe.entity.ArticleEntity;
-import com.fhdw.loeppe.entity.CustomerEntity;
 import com.fhdw.loeppe.repo.ArticleRepository;
-import com.fhdw.loeppe.repo.CustomerRepository;
-import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -18,17 +17,25 @@ public class ArticleRepositoryTest {
     @Autowired
     private ArticleRepository repository;
 
-    @Test
-    public void saveArticleSuccess(){
+    @BeforeEach
+    public void setUp() {
         ArticleEntity entity = new ArticleEntity("Taschentücher", "weiß", 1.10);
         repository.save(entity);
+    }
 
+    @Test
+    public void saveArticleSuccess(){
         var result = repository.findById(1L);
 
         assertTrue(result.isPresent());
-        assertEquals(result.get().getId(), 1L);
-        assertEquals(result.get().getName(), "Taschentücher");
-        assertEquals(result.get().getDescription(), "weiß");
-        assertEquals(result.get().getPrice(), 1.10);
+        assertThat(result.get().getId()).isEqualTo( 1L);
+        assertThat(result.get().getName()).isEqualTo( "Taschentücher");
+        assertThat(result.get().getDescription()).isEqualTo( "weiß");
+        assertThat(result.get().getPrice()).isEqualTo( 1.10);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        repository.deleteAll();
     }
 }
--- src/test/java/com/fhdw/loeppe/repositories/CustomerRepositoryTest.java
+++ src/test/java/com/fhdw/loeppe/repositories/CustomerRepositoryTest.java
@@ -2,11 +2,13 @@ package com.fhdw.loeppe.repositories;
 
 import com.fhdw.loeppe.entity.CustomerEntity;
 import com.fhdw.loeppe.repo.CustomerRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -15,17 +17,25 @@ public class CustomerRepositoryTest {
     @Autowired
     private CustomerRepository repository;
 
-    @Test
-    public void saveCustomerSuccess(){
+    @BeforeEach
+    public void setUp(){
         CustomerEntity entity = new CustomerEntity("John", "Doe", "Berlin");
         repository.save(entity);
+    }
 
-        var result = repository.findById(1L);
+    @Test
+    public void saveCustomerSuccess(){
+        var result = repository.findById(2L); //TODO: Kapselung der Tests; IDs werden fortlaufend vergeben
 
         assertTrue(result.isPresent());
-        assertEquals(result.get().getId(), 1L);
-        assertEquals(result.get().getFirstname(), "John");
-        assertEquals(result.get().getLastname(), "Doe");
-        assertEquals(result.get().getAddress(), "Berlin");
+        assertThat(result.get().getId()).isEqualTo( 2L); //TODO: Kapselung der Tests; IDs werden fortlaufend vergeben
+        assertThat(result.get().getFirstname()).isEqualTo( "John");
+        assertThat(result.get().getLastname()).isEqualTo( "Doe");
+        assertThat(result.get().getAddress()).isEqualTo( "Berlin");
+    }
+
+    @AfterEach
+    public void tearDown(){
+        repository.deleteAll();
     }
 }
