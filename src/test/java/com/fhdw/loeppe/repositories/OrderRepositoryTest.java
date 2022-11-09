7dd4731a5bd5eb86ae329b17fa7e91d7ab0c4899
Philip <klattephilip@gmail.com>
2022-11-09 16:48:18 +0100

    Kommentare entfernt

--- src/test/java/com/fhdw/loeppe/repositories/OrderRepositoryTest.java
+++ src/test/java/com/fhdw/loeppe/repositories/OrderRepositoryTest.java
@@ -18,7 +18,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-//@SpringBootTest
+@SpringBootTest
 public class OrderRepositoryTest {
 
     @Autowired
