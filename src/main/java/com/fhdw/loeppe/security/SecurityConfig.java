c749f9f328e6f44cc003f208b20f027b0b24d696
PhilipKlatte <klattephilip@gmail.com>
2023-01-14 18:10:32 +0100

    exclude /api/ from Security

--- src/main/java/com/fhdw/loeppe/security/SecurityConfig.java
+++ src/main/java/com/fhdw/loeppe/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -34,10 +35,16 @@ public class SecurityConfig extends VaadinWebSecurity {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/images/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/**").anonymous();
 
         super.configure(http);
 
         setLoginView(http, LoginView.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web){
+        web.ignoring().antMatchers("/api/**");
     }
 
     @Bean
