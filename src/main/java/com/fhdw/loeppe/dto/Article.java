7d1b273fe7289fa1ace78e994da825cb73f9101f
PhilipKlatte <klattephilip@gmail.com>
2023-01-14 19:00:22 +0100

    @NonNull bei Article und Order entfernt

--- src/main/java/com/fhdw/loeppe/dto/Article.java
+++ src/main/java/com/fhdw/loeppe/dto/Article.java
@@ -2,7 +2,6 @@ package com.fhdw.loeppe.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 import java.util.UUID;
 
@@ -12,13 +11,8 @@ public class Article {
 
     private UUID id;
 
-    @NonNull
     private String name;
-
-    @NonNull
     private String description;
-
-    @NonNull
     private Double price;
 
     public Article(String name) {
