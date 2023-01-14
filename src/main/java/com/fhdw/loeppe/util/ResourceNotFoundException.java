46c1d7860abf33536bfec3c59c86bf6d668054cc
PhilipKlatte <klattephilip@gmail.com>
2023-01-14 18:56:58 +0100

    RessourceNotFoundException

---
+++ src/main/java/com/fhdw/loeppe/util/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.fhdw.loeppe.util;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+}
